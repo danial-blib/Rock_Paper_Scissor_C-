7619464a16df2bee8743a730289ecf309a443c99
D0nial <115639494+danial-blib@users.noreply.github.com>
2024-10-21 22:36:35 +0500

    Add files via upload

---
+++ Rock_Paper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TicTakBeta
+{
+    class Tictak
+    {
+        static void Main()
+        {
+            string[] choices = {"rock", "paper", "scissor" };
+            Random random = new Random();
+            bool game = true;
+
+            while (game) 
+            {
+                Console.WriteLine("Choise: paper-rock-scissor");
+                string player = Console.ReadLine().ToLower();
+                string PC = choices[random.Next(choices.Length)];
+
+                Console.WriteLine($"PC choice: {PC}");
+
+                if (PC == player) 
+                {
+                    Console.WriteLine("-Tie!-");
+                }
+
+                else if ((player == "paper" && PC == "rock") ||
+                    (player == "rock" && PC == "scissor") ||
+                    (player == "scissor" && PC == "paper"))
+                {
+                    Console.WriteLine("-You win!-");
+                } else
+                {
+                    Console.WriteLine("-You Lose!-");
+                }
+
+                Console.WriteLine("Play again? y/n");
+                game = Console.ReadLine().ToLower() == "y";
+            }
+        }
+    }
+}
